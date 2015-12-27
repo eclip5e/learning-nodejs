80bf890252dc93f581a1aba01ab52e6bd0560104
eclip5e <viachaslau.akhrem@itechart-group.com>
2015-12-27 23:08:02 +0300

    [6] Additional work with modules, working in different modes, added logger module.

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var userFactory = require('./user');
+var db = require('db');
+db.connect();
 
-var kolya = new userFactory.User('Kolya');
-var borya = new userFactory.User('Borya');
+var log = require('logger')(module);
 
-kolya.greet();
+var UserFactory = require('./user');
+
+function initialize() {
+    var kolya = new UserFactory('Kolya');
+    var borya = new UserFactory('Borya');
+
+    kolya.greet();
+    borya.greet();
+
+    log(db.getTranslation('Initialization successful'));
+}
+
+if (module.parent) {
+    console.log('File [server.js] is required by some other module. Exporting functionality...');
+    exports.init = initialize;
+} else {
+    console.log('File [server.js] was called directly. Running initialization function...');
+    initialize();
+}
--- user/index.js
+++ user/index.js
@@ -1,11 +1,12 @@
-var i18n = require('./ru.json');
+var db = require('db');
+var log = require('logger')(module);
 
 function User(name) {
     this.name = name;
 }
 
 User.prototype.greet = function() {
-    console.log(i18n.Hello + '!, ' + this.name + '!');
+    log(db.getTranslation('Hello') + '!, ' + this.name + '!');
 };
 
-exports.User = User;
+module.exports = User;
