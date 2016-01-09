96033eb332cb1b6b554edf8014fc6f69d07dd241
eclip5e <viachaslau.akhrem@itechart-group.com>
2016-01-09 16:19:09 +0300

    Chat [31] Added configuration, logging and template system.

--- Chat/app.js
+++ Chat/app.js
@@ -1,46 +1,39 @@
+'use strict';
+
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
 
 //var routes = require('./routes');
 //var user = require('./routes/user');
 
 var app = express();
-app.set('port', 3000);
+app.set('port', config.get('port'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+    log.info('Express server listening on port ' + config.get('port'));
 });
 
 // Middleware
 
-app.use(function (req, res, next) {
-    if (req.url === '/') {
-        res.end('Hello');
-    } else {
-        next();
-    }
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
+        body: '<b>Hello!</b>'
+    });
 });
 
-app.use(function (req, res, next) {
-    if (req.url === '/forbidden') {
-        next(new Error('Access denied!'));
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res, next) {
-    if (req.url === '/test') {
-        res.end('Test');
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res) {
-    res.send(404, '404: Page not Found!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (err, req, res, next) {
     if (app.get('env') === 'development') {
@@ -50,19 +43,6 @@ app.use(function (err, req, res, next) {
         res.send(500);
     }
 });
-
-// all environments
-//app.set('port', process.env.PORT || 3000);
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//app.use(express.favicon());
-//app.use(express.logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded());
-//app.use(express.methodOverride());
-//app.use(express.session({ secret: 'your secret here' }));
-//app.use(app.router);
-//app.use(express.static(path.join(__dirname, 'public')));
 
 //app.get('/', routes.index);
 //app.get('/users', user.list);
