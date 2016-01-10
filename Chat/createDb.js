ba67b7d9367c68a85518bd576206156b9edc4d66
eclip5e <viachaslau.akhrem@itechart-group.com>
2016-01-10 17:08:56 +0300

    Chat [35] Working with Mongoose and MongoDB.

--- Chat/createDb.js
+++ Chat/createDb.js
@@ -1,17 +1,38 @@
 'use strict';
 
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+/*
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/chat';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+var schema = mongoose.Schema({
+    name: String
+});
 
-    var collection = db.collection('test_insert');
-    collection.insertOne({a:2}, function (error, docs) {
-        db.close();
+schema.methods.meow = function () {
+    console.log(this.get('name') + ' says: Meoooooowwww!');
+};
+
+var Cat = mongoose.model('Cat', schema);
+
+var kitty = new Cat({ name: 'Zildjian' });
+
+kitty.save(function (err, kitty, affected) {
+    kitty.meow();
+});
+*/
+
+var User = require('models/user').User;
+
+var user = new User({
+    username: 'Tester3',
+    password: '1111'
+
+});
+
+user.save(function (error, user, affected) {
+    console.log(arguments);
+
+    User.findOne({username: 'Tester'}, function (err, tester) {
+        console.log(tester);
     });
 });
